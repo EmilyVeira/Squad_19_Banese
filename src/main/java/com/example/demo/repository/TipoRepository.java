9037cfe050de9278c657f6b9d042b622c5e694af
EmilyVeira <134454554+EmilyVeira@users.noreply.github.com>
2024-05-23 15:53:34 -0300

    Create TipoRepository.java

---
+++ src/main/java/com/example/demo/repository/TipoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Tipo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoRepository extends JpaRepository<Tipo, Integer> {
+}
