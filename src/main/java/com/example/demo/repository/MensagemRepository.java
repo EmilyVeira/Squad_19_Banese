9f2a899ed1fc644057166d37b1da5dd3b388c64b
EmilyVeira <134454554+EmilyVeira@users.noreply.github.com>
2024-05-23 15:52:54 -0300

    Update MensagemRepository.java

--- src/main/java/com/example/demo/repository/MensagemRepository.java
+++ src/main/java/com/example/demo/repository/MensagemRepository.java
@@ -1,13 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Mensagem;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MensagemRepository extends JpaRepository<Mensagem,Integer> {
-	List<Mensagem> findAllByEnviadaFalse();
+public interface MensagemRepository extends JpaRepository<Mensagem, Integer> {
 }
